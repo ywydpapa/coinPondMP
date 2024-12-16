9c23ffef67d6f13bdec33bd5ab46daee9cc2876b
DongJin Kim <31959020+ywydpapa@users.noreply.github.com>
2024-12-16 15:34:22 +0900

    멀티프로세싱

--- main.py
+++ main.py
@@ -11,20 +11,19 @@ def heavy_work(name):
 
 if __name__ == '__main__':
     import concurrent.futures
-
-    start = time.time()
-
-    total_result = 0
-    pool = concurrent.futures.ProcessPoolExecutor(max_workers=40)
-
-    procs = []
-    for i in range(40):
-        procs.append(pool.submit(heavy_work, i))
-
-    for p in concurrent.futures.as_completed(procs):
-        total_result += p.result()
-
-    end = time.time()
-
-    print("수행시간: %f 초" % (end - start))
-    print("총결괏값: %s" % total_result)
+    cnt = 0
+    while True:
+        cnt += 1
+        start = time.time()
+        total_result = 0
+        pool = concurrent.futures.ProcessPoolExecutor(max_workers=10)
+        procs = []
+        for i in range(4):
+            procs.append(pool.submit(heavy_work, i))
+        for p in concurrent.futures.as_completed(procs):
+            total_result += p.result()
+        end = time.time()
+        print("수행시간: %f 초" % (end - start))
+        print("총결과값: %s" % total_result)
+        print("구동 횟수 : ", str(cnt))
+        time.sleep(5)
